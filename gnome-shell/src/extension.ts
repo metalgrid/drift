44a86475fee984085c8c253dd7d7f8e3f924daa3
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2024-11-28 20:12:52 +0200

    extension overhaul

--- gnome-shell/src/extension.ts
+++ gnome-shell/src/extension.ts
@@ -1,97 +1,212 @@
-import { DriftService } from "./types";
+import { ButtonProps, DriftService } from "./types";
 import "@girs/gjs";
+import "@girs/gjs/dom";
 
 import $t from "@girs/st-15";
 import Gio from "@girs/gio-2.0";
+import GLib from "@girs/glib-2.0";
 import GObject from "@girs/gobject-2.0";
 
 import {
   Extension,
   gettext as _,
 } from "@girs/gnome-shell/extensions/extension";
-import * as PanelMenu from "@girs/gnome-shell/ui/panelMenu";
 import * as Main from "@girs/gnome-shell/ui/main";
+import {
+  Notification,
+  NotificationApplicationPolicy,
+  NotificationDestroyedReason,
+  Source,
+} from "@girs/gnome-shell/ui/messageTray";
+import * as PanelMenu from "@girs/gnome-shell/ui/panelMenu";
 
 const serviceName = "com.github.metalgrid.Drift";
 const objPath = "/com/github/metalgrid/Drift";
 const ifaceName = "com.github.metalgrid.Drift";
+const sigNotify = "Notify";
+const sigQuestion = "Question";
+class PersistentNotifierType extends Source {
+  public override destroy(_reason: NotificationDestroyedReason): void {}
+
+  public sendNotification(title: string, body: string, icon?: string): void {
+    const notification = new Notification({
+      source: this,
+      title: title,
+      body: body,
+    });
+
+    if (icon) {
+      notification.gicon = new Gio.ThemedIcon({ name: icon });
+    }
+
+    this.addNotification(notification);
+  }
+
+  public sendActionableNotification(
+    title: string,
+    body: string,
+    actions: { [label: string]: () => void }
+  ): void {
+    const notification = new Notification({
+      source: this,
+      title: title,
+      body: body,
+    });
+
+    for (const [label, callback] of Object.entries(actions)) {
+      notification.addAction(label, () => {
+        callback();
+        notification.destroy();
+      });
+    }
+
+    this.addNotification(notification);
+  }
+
+  public _destroy(reason: NotificationDestroyedReason): void {
+    return super.destroy(reason);
+  }
+}
+
+const PersistentNotifier = GObject.registerClass(PersistentNotifierType);
+
+function createProxy(
+  bus: Gio.DBusConnection,
+  service: string,
+  path: string,
+  schema: string
+): DriftService {
+  const DriftProxyFactory = Gio.DBusProxy.makeProxyWrapper(schema);
+  return new (DriftProxyFactory as any)(bus, service, path);
+}
 
 const Drift = GObject.registerClass(
   class Drift extends PanelMenu.Button {
-    private _driftProxy: DriftService | null = null;
-    // nasty hacks
-    public _path: string = "";
+    private driftService: DriftService;
+    private bus = Gio.DBus.session;
+    public extDir: Gio.File;
+    private notifier: PersistentNotifierType;
+    private subscriptions: number[] = [];
 
-    override _init(
-      params?: Partial<PanelMenu.ButtonBox.ConstructorProps> & { path: string }
-    ): void;
+    public constructor(path: Gio.File, args: ButtonProps) {
+      //@ts-expect-error // Smells like a TS bug. ButtonProps *is* a union of tuples.
+      super(...args);
+      this.extDir = path;
 
-    override _init(
-      menuAlignment: number,
-      nameText: string,
-      dontCreateMenu?: boolean
-    ): void;
+      const [ok, buf] = GLib.file_get_contents(
+        `${this.extDir.get_path()}/schema/${serviceName}.xml`
+      );
+      if (!ok) {
+        logError(
+          new Error(
+            `Failed to load schema for ${serviceName} from ${this.extDir}`
+          )
+        );
+        throw new Error(`Failed to load schema for ${serviceName}`);
+      }
+      this.driftService = createProxy(
+        Gio.DBus.session,
+        ifaceName,
+        objPath,
+        new TextDecoder().decode(buf)
+      );
 
-    override _init(
-      _p1?: Partial<PanelMenu.ButtonBox.ConstructorProps> | number,
-      _p2?: string,
-      _p3?: boolean
-    ): void {
-      super._init(...arguments);
-      this.init();
-    }
+      this.notifier = new PersistentNotifier({
+        title: _("Drift"),
+        icon: new Gio.ThemedIcon({ name: "mail-unread-symbolic" }),
+        policy: new NotificationApplicationPolicy(serviceName),
+      });
 
-    init() {
       const icon = new $t.Icon({
         iconName: "send-to-symbolic",
         styleClass: "system-status-icon",
       });
-
       this.add_child(icon);
-      this.setupDBus();
-    }
 
-    setupDBus() {
-      const bus = Gio.DBus.session;
-
-      bus.watch_name(
+      this.bus.watch_name(
         serviceName,
         Gio.BusNameWatcherFlags.NONE,
-        () => this._serviceOnline(),
-        () => this._serviceOffline()
+        this.driftOnline.bind(this),
+        this.driftOffline.bind(this)
       );
     }
 
-    _serviceOnline() {
-      log("DBus service online");
-
-      // should be read from the bundled schema...
-      const schema = `
-      <node>
-    <interface name="com.github.metalgrid.Drift">
-        <method name="ListPeers">
-            <arg type="as" direction="out"></arg>
-        </method>
-        <method name="Request">
-            <arg type="s" direction="in"></arg>
-            <arg type="s" direction="in"></arg>
-        </method>
-    </interface>
-</node>
-      `;
-
-      const DriftProxy = Gio.DBusProxy.makeProxyWrapper(schema);
-      this._driftProxy = new (DriftProxy as any)(
-        Gio.DBus.session,
+    driftOnline() {
+      let subId = this.bus.signal_subscribe(
         serviceName,
-        objPath
+        ifaceName,
+        sigNotify,
+        objPath,
+        null,
+        Gio.DBusSignalFlags.NONE,
+        (
+          _connection,
+          _senderName,
+          _objectPath,
+          _interfaceName,
+          _signalName,
+          parameters
+        ) => {
+          log("Handling notify signal");
+          const [message] = parameters.recursiveUnpack();
+          this.notifier.sendNotification("Drift", message);
+        }
       );
-      const peers = this._driftProxy?.ListPeersSync();
-      log("Drifters: ", peers);
+      this.subscriptions.push(subId);
+
+      subId = this.bus.signal_subscribe(
+        serviceName,
+        ifaceName,
+        sigQuestion,
+        objPath,
+        null,
+        Gio.DBusSignalFlags.NONE,
+        (
+          _connection,
+          _senderName,
+          _objectPath,
+          _interfaceName,
+          _signalName,
+          parameters
+        ) => {
+          const [id, message] = parameters.recursiveUnpack();
+
+          this.notifier.sendActionableNotification("Drift", message, {
+            Accept: () => {
+              this.driftService.RespondSync(id, "ACCEPT");
+            },
+            Decline: () => {
+              this.driftService.RespondSync(id, "DECLINE");
+            },
+          });
+
+          Main.notify("Drift", `Question ${id}: ${message}`);
+        }
+      );
+      this.subscriptions.push(subId);
+
+      this.connect("button-press-event", () => {
+        const [peers] = this.driftService.ListPeersSync();
+        log(`${peers.length} peers found`);
+        peers.forEach((peer) => {
+          log(`Peer: ${peer}`);
+        });
+      });
     }
 
-    _serviceOffline() {
+    driftOffline() {
       log("DBus service offline");
+      this.subscriptions.forEach((subId) => {
+        this.bus.signal_unsubscribe(subId);
+      });
+    }
+
+    override destroy(): void {
+      if (this.notifier) {
+        this.notifier._destroy(NotificationDestroyedReason.SOURCE_CLOSED);
+      }
+
+      super.destroy();
     }
   }
 );
@@ -100,8 +215,11 @@ export default class DriftExtension extends Extension {
   private extension: any = null;
 
   override enable(): void {
-    this.extension = new Drift(0, _(this.metadata.name), false);
-    this.extension._path = this.metadata.path;
+    this.extension = new Drift(this.metadata.dir, [
+      0,
+      _(this.metadata.name),
+      false,
+    ]);
     Main.panel.addToStatusArea(this.uuid, this.extension);
   }
 
--- gnome-shell/src/types.ts
+++ gnome-shell/src/types.ts
@@ -1,6 +1,15 @@
+// Type definitions for the Drift service
+import { Button } from "@girs/gnome-shell/ui/panelMenu";
+
+export type ButtonProps =
+  | [Partial<Button.ConstructorProps>]
+  | [menuAlignment: number, nameText: string, dontCreateMenu?: boolean];
+
 export interface DriftService {
-  ListPeersSync(): [string];
-  ListPeersAsync(): Promise<[string]>;
+  ListPeersSync(): [string[]];
+  ListPeersAsync(): Promise<[string[]]>;
   RequestSync(peer: string, file: string): void;
   RequestAsync(peer: string, file: string): Promise<void>;
+  RespondSync(id: string, answer: "ACCEPT" | "DECLINE"): void;
+  RespondAsync(id: string, answer: "ACCEPT" | "DECLINE"): Promise<void>;
 }
